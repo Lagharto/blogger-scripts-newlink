98dfef192a2761c8ec214d23bd8a0ceefcd1a1fd
Lagharto <lagharto.zone@gmail.com>
2025-12-15 18:04:44 -0500

    Create reemplazo-newlink.js

---
+++ reemplazo-newlink.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="https://shrinkme.site/"]').forEach(function(link) {
+    link.href = link.href.replace(
+      "https://shrinkme.site/",
+      "https://shrinkme-redirect.pages.dev/"
+    );
+  });
+});
